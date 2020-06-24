4b0c9917b0b1414f09efbe880fb7ed58d8fe536d
Joshi <rohan.joshi@capgemini.com>
2020-06-24 21:11:41 +0530

    Integration Test added

---
+++ src/test/java/net/codejava/service/TntegrationTest.java
@@ -0,0 +1,34 @@
+package net.codejava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+
+import net.codejava.controller.AppController;
+
+@RunWith(SpringRunner.class)
+public class TntegrationTest {
+	
+	@InjectMocks
+	private AppController appController;
+
+	@Mock
+	private ProductService service;
+
+	@Mock
+	Model model;
+
+	@Test
+	public void viewHomePageIntegrationTest() {
+		System.setProperty("webdriver.chrome.driver","C:\\BACKUP\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8081");
+	}
+
+
+}
