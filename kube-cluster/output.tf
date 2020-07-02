5afc1a6b51f29d37dfdd89a200188019f6e7521a
rohanjoshi95 <39187238+rohanjoshi95@users.noreply.github.com>
2020-07-02 22:51:52 +0530

    Update output.tf

--- kube-cluster/output.tf
+++ kube-cluster/output.tf
@@ -1,6 +1,6 @@
-output "Master_public_ip" {
+output "Stage_Master_public_ip" {
   value = aws_instance.my-test-instance.public_ip
 }
-output "Worker_public_ip" {
+output "Stage_Worker_public_ip" {
   value = aws_instance.my-test-instance2.public_ip
 }
